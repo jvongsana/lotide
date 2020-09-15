4ce6f5f3b797e45d4ca2deb57420411fc03da091
Jason Vongsana <jvongsana@hotmail.com>
2020-09-15 23:18:57 +0000

    Returns an array without specified items

---
+++ without.js
@@ -0,0 +1,43 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arrOne, arrTwo) {
+  if (eqArrays(arrOne, arrTwo)) {
+    console.log("These arrays are equal.");
+  } else {
+    console.log("These arrays are not equal.");
+  }
+};
+
+
+const without = function(source, itemsToRemove) {
+  let withoutArr = [...source];
+
+  for (let i = 0; i < source.length; i++) {
+    for (let j = 0; j < itemsToRemove.length; j++) {
+      if (source[i] === itemsToRemove[j]) {
+        withoutArr.splice(i, 1);
+      }
+    }
+  }
+
+  return withoutArr;
+};
+
+
+// without([1, 2, 3], [1]) // => [2, 3]
+// without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
+
+// const words = ["hello", "world", "lighthouse"];
+// without(words, ["lighthouse"]); // no need to capture return value for this test case
+// // Make sure the original array was not altered by the without function
+// assertArraysEqual(words, ["hello", "world", "lighthouse"]);
