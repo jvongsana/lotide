a5ed5834558055f895afe7f8e0c47e6eb0d771c5
Jason Vongsana <jvongsana@hotmail.com>
2020-09-16 20:57:39 +0000

    Return an object containing counts of everything that the input object listed

--- countOnly.js
+++ countOnly.js
@@ -13,9 +13,13 @@ const countOnly = function(allItems, itemsToCount) {
   const results = {};
 
   for (const item of allItems) {
-    console.log(item);
+    if (results[item]) {
+      results[item] += 1;
+    } else {
+      results[item] = 1;
+    }
   }
-  
+
 
   return results;
 };
